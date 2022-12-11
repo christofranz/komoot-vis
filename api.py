a0974862cc5095e2d832b40df488fe7b47df178d
christofranz <franzke.christoph@web.de>
2022-12-11 11:44:51 +0100

    Add docstrings to api.py

--- api.py
+++ api.py
@@ -17,6 +17,10 @@ class BasicAuthToken(requests.auth.AuthBase):
 
 
 class KomootApi:
+    """
+    Class for login in to Komoot and fetching the tour data.
+    """
+
     def __init__(self):
         self.user_id = ""
         self.token = ""
@@ -34,6 +38,9 @@ class KomootApi:
 
     @staticmethod
     def __send_request(url, auth, critical=True):
+        """
+        General function to make api calls with basic authentication.
+        """
         r = requests.get(url, auth=auth)
         if r.status_code != 200:
             print("Error " + str(r.status_code) + ": " + str(r.json()))
@@ -42,6 +49,16 @@ class KomootApi:
         return r
 
     def login(self, email, password):
+        """
+        Login to Komoot API.
+
+        Args:
+        email (str): E-Mail address of the user
+        password (str): Password of the user as string
+
+        Returns:
+        -
+        """
         print("Logging in...")
 
         r = self.__send_request(
@@ -55,6 +72,16 @@ class KomootApi:
         print("Logged in as '" + r.json()["user"]["displayname"] + "'")
 
     def fetch_tours(self, tourType="all", silent=False):
+        """
+        Fetch high-level data of all Komoot tours.
+
+        Args:
+        tourType (str): Type of the tours to fetch (e.g. Hiking)
+        silent (boolean): If silent, progress will not be outputted
+
+        Returns:
+        results (dict): All tours with id and further meta data
+        """
         if not silent:
             print("Fetching tours of user '" + self.user_id + "'...")
 
@@ -65,10 +92,13 @@ class KomootApi:
             BasicAuthToken(self.user_id, self.token),
         )
 
-        results = {}
+        # store tour response in json
         tours = r.json()["_embedded"]["tours"]
         with open("result.json", "w") as f:
             json.dump(tours, f)
+
+        # create dict with tour id and metadata
+        results = {}
         for tour in tours:
             if tourType != "all" and tourType != tour["type"]:
                 continue
@@ -86,6 +116,15 @@ class KomootApi:
         return results
 
     def fetch_tour(self, tour_id):
+        """
+        Fetch low-level data of a specific Komoot tour.
+
+        Args:
+        tour_id (str): Valid id of one of the Komoot tours
+
+        Returns:
+        (json): Tour details including coordinates, highlights etc.
+        """
         print("Fetching tour '" + tour_id + "'...")
 
         r = self.__send_request(
@@ -103,6 +142,15 @@ class KomootApi:
         return r.json()
 
     def fetch_highlight_tips(self, highlight_id):
+        """
+        Fetch highlight data of a specific Komoot tour.
+
+        Args:
+        highlight_id (str): Valid id of one of the highlights
+
+        Returns:
+        (json): Highlight details
+        """
         print("Fetching highlight '" + highlight_id + "'...")
 
         r = self.__send_request(
